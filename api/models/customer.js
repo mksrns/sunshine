6bb57b57b02ea8760135d2c6a34432d2e1b8c651
Monu Kumar <monukumar.1rn13cs050@gmail.com>
2019-08-24 14:33:28 +0530

    initial commit

---
+++ api/models/customer.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var customerSchema = new Schema({
+    name: {
+        firstname: {type: String, required: true},
+        lastname: {type: String}
+    },
+    customerImage: { type: String },
+    username: {type: String, required:true, unique: true},
+    password: {type: String, required:true },
+    created_at: {type: Date, required:true },
+    updated_at: {type: Date},
+    contactInfo:{
+        telephone: [String],
+        email: {type: String},
+        address: { 
+            street: {type: String},
+            city: {type: String},
+            state: {type: String},
+            pincode: {type: Number},
+        }
+    },
+    /* order: {
+        date: {type: Date},
+        invoiceNumber: {type: String},
+        paid: {type: Number},
+        pending: {type: Number},
+        description: {type: String},
+    } */
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database:'mongodb://localhost:27017/wingsacademy',
+    secret: 'mysecret'
+    //database:'mongodb://monu007:monu007@ds147436.mlab.com:47436/wingsacademy'
+}
